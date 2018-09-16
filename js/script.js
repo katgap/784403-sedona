0f93d9a81c15c435021f708359393bef32677c09
katgap <k-gaponova@mail.ru>
2018-09-16 17:27:09 +0300

    Завершение верстки и добавление js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var link = document.querySelector(".reservation_button_brown");
+var form = document.querySelector(".reservation_form");
+var arrivalDate = form.querySelector("[name=arrival-date]");
+var departureDate = form.querySelector("[name=departure-date]");
+var adultsAmount = form.querySelector("[name=amount-adults]");
+var childrenAmount = form.querySelector("[name=amount-children]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form.classList.toggle("reservation_form_show");
+  form.classList.add("reservation_form_animation");
+  form.classList.remove("reservation_form_error");
+  arrivalDate.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !departureDate.value || !adultsAmount.value || !childrenAmount.value) {
+    evt.preventDefault();
+    form.classList.remove("reservation_form_error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("reservation_form_error");
+  }
+});
