431f1e3f6dda90fe29d642a0ba75559e414d421a
katgap <k-gaponova@mail.ru>
2018-09-16 20:49:49 +0300

    Поправила вёрстку

--- js/script.js
+++ js/script.js
@@ -6,18 +6,27 @@ var adultsAmount = form.querySelector("[name=amount-adults]");
 var childrenAmount = form.querySelector("[name=amount-children]");
 
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  form.classList.toggle("reservation_form_show");
-  form.classList.add("reservation_form_animation");
-  form.classList.remove("reservation_form_error");
-  arrivalDate.focus();
+    evt.preventDefault();
+    form.classList.toggle("reservation_form_show");
+    form.classList.remove("reservation_form_error");
+    arrivalDate.focus();
 });
 
 form.addEventListener("submit", function (evt) {
-  if (!arrivalDate.value || !departureDate.value || !adultsAmount.value || !childrenAmount.value) {
-    evt.preventDefault();
-    form.classList.remove("reservation_form_error");
-    form.offsetWidth = form.offsetWidth;
-    form.classList.add("reservation_form_error");
-  }
+    if (!arrivalDate.value || !departureDate.value || !adultsAmount.value || !childrenAmount.value) {
+        evt.preventDefault();
+        form.classList.remove("reservation_form_error");
+        form.offsetWidth = form.offsetWidth;
+        form.classList.add("reservation_form_error");
+    }
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (form.classList.contains("reservation_form_show")) {
+            form.classList.remove("reservation_form_show");
+            form.classList.remove("reservation_form_error");
+        }
+    }
 });
